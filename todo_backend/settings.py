660fe4d66aa906b5ba076c74e9175783ed0f6b91
Dong Geon Brian Park <brianpark@Dongui-MacBook-Pro.local>
2021-06-21 17:05:57 -0400

    cors

--- todo_backend/settings.py
+++ todo_backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-!3*f(q*b@p5a8en81*^t()zuu(r2)u*m=n90p6(vnbb3yw7z+d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
